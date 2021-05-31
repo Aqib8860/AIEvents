aa29760bb4e3ab6db578cd3aa7e8d6b288f1ce7b
Aqib Khan <67043041+Aqib8860@users.noreply.github.com>
2021-05-31 16:57:47 +0530

    Update settings.py

--- AIEventCompany/settings.py
+++ AIEventCompany/settings.py
@@ -132,7 +132,7 @@ AUTH_USER_MODEL = 'core.User'
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
